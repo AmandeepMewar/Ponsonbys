c819129e0748b87e1d3389fdad7521c8d84d3fce
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-03 23:01:53 +0530

    feat: implement signup auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,32 @@
+import User from '../models/user.model.js';
+
 export async function signup(req, res) {
-  res.send('Sign up route called!');
+  const { name, email, password, passwordConfirm } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists)
+      return res
+        .status(400)
+        .json({ status: 'fail', message: 'User already exists' });
+
+    const user = await User.create({ name, email, password, passwordConfirm });
+
+    res.status(201).json({
+      status: 'success',
+      result: {
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+        },
+      },
+      message: 'User created successfully',
+    });
+  } catch (error) {
+    res.status(500).json({ status: 'error', message: error.message });
+  }
 }
 
 export async function login(req, res) {
