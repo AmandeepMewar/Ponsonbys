a943e749402e6775207b4491aab3ae5a26c983f3
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-05 22:50:31 +0530

    feat: implement createProduct functionality in product controller

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,9 +1,34 @@
 import Product from '../models/product.model.js';
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
 
 export async function getAllProducts(req, res) {
   try {
     const products = await Product.find();
     res.status(200).json({ status: 'success', result: products });
+  } catch (error) {
+    res.status(500).json({ status: 'error', message: error.message });
+  }
+}
+
+export async function createProduct(req, res) {
+  try {
+    const { name, description, price, category } = req.body;
+
+    const image = req.file.path;
+    console.log(image);
+
+    const cloudinaryResponse = await uploadOnCloudinary(image);
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryResponse?.secure_url
+        ? cloudinaryResponse?.secure_url
+        : '',
+      category,
+    });
+
+    res.status(201).json({ status: 'success', result: product });
   } catch (error) {
     res.status(500).json({ status: 'error', message: error.message });
   }
--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { protectRoute, adminRoute } from '../middlewares/auth.middleware.js';
-import { getAllProducts } from '../controllers/product.controller.js';
+import {
+  getAllProducts,
+  createProduct,
+} from '../controllers/product.controller.js';
+import { UploadImage } from '../middlewares/multer.middleware.js';
 
 const router = express.Router();
 
 router.get('/', protectRoute, adminRoute, getAllProducts);
-
+router.post('/', protectRoute, adminRoute, UploadImage, createProduct);
 export default router;
