191b08b1078efba2d87ef8c3a1c92731552b1d9d
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-05 22:48:56 +0530

    feat: implement Multer middleware

---
+++ backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,32 @@
+import multer from 'multer';
+import path from 'path';
+
+const multerStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/temp');
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const filename = `image_${req?.body?.name.replace(
+      ' ',
+      ''
+    )}_${Date.now()}${ext}`;
+    console.log(filename);
+    cb(null, filename);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Not an image! Please upload only images'), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+export const UploadImage = upload.single('image');
