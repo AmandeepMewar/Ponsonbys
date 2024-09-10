f5239dd8041bac9cdd56ced24729db15f4f44e3b
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-10 13:31:30 +0530

    feat: add initial React Router setup

---
+++ frontend/src/ui/AppLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+export default function AppLayout() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
