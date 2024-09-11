5d5ec457a278c9161feba022723000031f2c45dc
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-12 00:59:27 +0530

    feat: implement CORS to allow cross-origin API access

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,9 +7,11 @@ import cartRoutes from './routes/cart.routes.js';
 import couponRoutes from './routes/coupon.routes.js';
 import paymentRoutes from './routes/payment.routes.js';
 import analyticsRoutes from './routes/analytics.routes.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
