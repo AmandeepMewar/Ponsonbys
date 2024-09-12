6b886ccf52d8fcff46ddff424a6f39efea5173fd
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-12 12:39:22 +0530

    chore: install and configure react-hot-toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import AppLayout from './ui/AppLayout';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,6 +28,20 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position='top-center'
+        gutter={8}
+        toastOptions={{
+          className:
+            'max-w-md px-4 py-3 bg-yellow-50 text-yellow-900 text-center',
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
