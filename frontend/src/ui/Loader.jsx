8e5bb736908de981b9bbfdf28816538f7b4978c4
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-13 14:52:12 +0530

    feat: add spinner loader

---
+++ frontend/src/ui/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className='fixed left-0 top-0 flex min-h-screen w-full items-center justify-center backdrop-blur-[4px]'>
+      <div className='loader' />;
+    </div>
+  );
+}
