336e9249d2bc6f6bc03e9f249dbd8f1f23d5dbaf
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-13 15:04:24 +0530

    feat: update classes in Button

--- frontend/src/ui/Button.jsx
+++ frontend/src/ui/Button.jsx
@@ -1,9 +1,9 @@
-export default function Button({ children, ...props }) {
+export default function Button({ children, className, ...props }) {
+  const classes =
+    className ||
+    'flex w-full justify-center rounded-md border border-transparent bg-yellow-700 px-4 py-2 text-sm font-medium text-white shadow-sm transition duration-150 ease-in-out hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 disabled:opacity-50';
   return (
-    <button
-      {...props}
-      className='flex w-full justify-center rounded-md border border-transparent bg-yellow-700 px-4 py-2 text-sm font-medium text-white shadow-sm transition duration-150 ease-in-out hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 disabled:opacity-50'
-    >
+    <button {...props} className={classes}>
       {children}
     </button>
   );
