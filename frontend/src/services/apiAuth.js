4c70bc5bd233d96e64df26d128fc6488b05787ba
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-13 15:02:18 +0530

    feat: implement getProfile and logout in apiAuth

--- frontend/src/services/apiAuth.js
+++ frontend/src/services/apiAuth.js
@@ -11,6 +11,7 @@ export async function signup(user) {
     throw new Error(
       data.message || 'An Error occurred please try again later.'
     );
+
   return data;
 }
 
@@ -26,6 +27,28 @@ export async function login(user) {
   const data = await response.json();
   if (!response.ok)
     throw new Error(data.message || 'An error occurred. Please try again.');
+
+  return data;
+}
+
+export async function getProfile() {
+  const response = await fetch('/api/auth/profile');
+
+  const data = await response.json();
+  if (!response.ok) {
+    return null;
+  }
+  return data.result.user;
+}
+
+export async function logout() {
+  const response = await fetch('/api/auth/logout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
 
   return data;
 }
