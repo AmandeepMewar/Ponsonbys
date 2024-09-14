2637bf37a2e7d61666f3293d1c63997fa05871cf
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-14 16:46:29 +0530

    feat: implement useProducts hook

---
+++ frontend/src/features/dashboard/useProducts.js
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+import { getAllProducts as getAllProductsApi } from '../../services/apiProduct';
+
+export function useProducts() {
+  const { data, isPending: isLoading } = useQuery({
+    queryKey: ['products'],
+    queryFn: getAllProductsApi,
+  });
+
+  return { data, isLoading };
+}
