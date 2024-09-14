6984d760ebbcad1d533e555c3089f45075f4b327
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-14 16:48:01 +0530

    feat: implement createProductForm functionality in the component

--- frontend/src/features/dashboard/CreateProductForm.jsx
+++ frontend/src/features/dashboard/CreateProductForm.jsx
@@ -4,6 +4,7 @@ import Button from '../../ui/Button';
 import FormInput from '../../ui/FormInput';
 import Input from '../../ui/Input';
 import InputIcon from '../../ui/InputIcon';
+import { useCreateProduct } from './useCreateProduct';
 
 const categories = [
   'jeans',
@@ -16,10 +17,14 @@ const categories = [
 ];
 
 export default function CreateProductForm() {
-  const { register, handleSubmit, formState } = useForm();
+  const { createProduct, isLoading } = useCreateProduct();
+  const { register, handleSubmit, formState, reset } = useForm();
   const { errors } = formState;
 
-  function onSubmit() {}
+  function onSubmit(data) {
+    const image = data.image[0];
+    createProduct({ ...data, image }, { onSettled: reset() });
+  }
 
   return (
     <div className='flex w-full flex-col justify-center rounded-md bg-yellow-100 px-16 py-12 sm:w-2/3 lg:w-2/5'>
@@ -34,6 +39,7 @@ export default function CreateProductForm() {
             type='text'
             placeholder='Nike Shoes'
             id='productName'
+            disabled={isLoading}
             {...register('productName', {
               required: 'This field is required',
             })}
@@ -49,6 +55,7 @@ export default function CreateProductForm() {
             placeholder='A luxury nike shoes'
             rows='3'
             name='description'
+            disabled={isLoading}
             className='block w-full rounded-md border border-yellow-800 bg-yellow-50 px-10 py-2 placeholder-yellow-700 shadow-sm focus:border-orange-600 focus:outline-none focus:ring-yellow-900 sm:text-sm'
             {...register('description', {
               required: 'This field is required',
@@ -61,6 +68,7 @@ export default function CreateProductForm() {
             type='number'
             id='price'
             step='0.01'
+            disabled={isLoading}
             placeholder='2000'
             {...register('price', { required: 'This field is required' })}
           />
@@ -74,6 +82,7 @@ export default function CreateProductForm() {
           <select
             id='category'
             name='category'
+            disabled={isLoading}
             className='mt-1 block w-full rounded-md border border-yellow-100 bg-orange-600 px-3 py-2 text-yellow-50 shadow-sm focus:border-yellow-200 focus:outline-none focus:ring-2 focus:ring-yellow-100'
             {...register('category', { required: 'This field is required' })}
           >
@@ -92,12 +101,20 @@ export default function CreateProductForm() {
             type='file'
             id='image'
             accept='image/*'
+            disabled={isLoading}
             {...register('image', { required: 'This field is required' })}
           />
         </FormInput>
 
-        <Button type='submit'>
-          <PackagePlus className='mr-2 h-5 w-5' /> Create Product
+        <Button type='submit' disabled={isLoading}>
+          {isLoading ? (
+            <span>Loading...</span>
+          ) : (
+            <>
+              <PackagePlus className='mr-2 h-5 w-5' />{' '}
+              <span>Create Product</span>
+            </>
+          )}
         </Button>
       </form>
     </div>
