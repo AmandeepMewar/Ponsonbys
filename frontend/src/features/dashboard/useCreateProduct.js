049159c2b206845c32afb71e175662b63b29e039
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-14 16:45:55 +0530

    feat: implement useCreateProduct hook

---
+++ frontend/src/features/dashboard/useCreateProduct.js
@@ -0,0 +1,20 @@
+import { useMutation } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { createProduct as createProductApi } from '../../services/apiProduct';
+
+export function useCreateProduct() {
+  const navigate = useNavigate();
+  const { mutate: createProduct, isPending: isLoading } = useMutation({
+    mutationFn: createProductApi,
+    onSuccess: (data) => {
+      toast.success('Product created successfully!');
+      navigate('/dashboard/products');
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { createProduct, isLoading };
+}
