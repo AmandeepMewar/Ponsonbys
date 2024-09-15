f1f4f1ae765a477741244e75aa85fff387731909
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-15 17:41:29 +0530

    feat: implement useToggleFeaturedProduct hook

---
+++ frontend/src/features/dashboard/useToggleFeaturedProduct.js
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
+import { toggleFeaturedProduct as toggleFeaturedProductApi } from '../../services/apiProduct';
+
+export function useToggleFeaturedProduct() {
+  const queryClient = useQueryClient();
+  const { mutate: toggleFeaturedProduct, isPending: isLoading } = useMutation({
+    mutationFn: toggleFeaturedProductApi,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ['products'],
+      });
+      toast.success(`${data.name} is featured now!`);
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { toggleFeaturedProduct, isLoading };
+}
