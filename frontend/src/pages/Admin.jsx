c9216888d6c35a7f2ae1842c29ec9d766977efe7
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-15 17:40:19 +0530

    feat: add admin protection to AdminPage

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,7 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useProfile } from '../features/authentication/useProfile';
 import Dashboard from '../features/dashboard/Dashboard';
-
 export default function Admin() {
+  const navigate = useNavigate();
+  const { user } = useProfile();
+
+  useEffect(
+    function () {
+      if (user?.role !== 'admin') navigate('/');
+    },
+    [user, navigate]
+  );
   return (
     <div className='relative min-h-screen overflow-hidden'>
       <Dashboard />
