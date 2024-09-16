f8665a85e0588b1e35da46472f22291cd3a736f1
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-17 00:03:29 +0530

    style: update Button styles and add margin to ProductList component

--- frontend/src/features/dashboard/ProductList.jsx
+++ frontend/src/features/dashboard/ProductList.jsx
@@ -3,7 +3,7 @@ import ProductTable from './ProductTable';
 export default function ProductList() {
   return (
     <motion.div
-      className='mx-auto max-w-4xl overflow-hidden rounded-lg bg-yellow-100 shadow-lg'
+      className='mx-auto mb-12 max-w-4xl overflow-hidden rounded-lg bg-yellow-100 shadow-lg'
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
