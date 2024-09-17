299b829cb1185b3f2218cac840779d20e1cb1b11
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-17 22:56:03 +0530

    feat: add removeFromCart function in apiCart

--- frontend/src/services/apiCart.js
+++ frontend/src/services/apiCart.js
@@ -23,3 +23,19 @@ export async function addToCart(productId) {
 
   return data;
 }
+
+export async function removeFromCart(productId) {
+  const response = await fetch('/api/cart', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ productId: productId }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) throw new Error(data.message);
+
+  return data;
+}
