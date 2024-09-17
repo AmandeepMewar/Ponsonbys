9534b4f2e5cff8d507389af2290ba560051d9d9c
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-17 16:20:47 +0530

    feat: implement useAddToCart hook

---
+++ frontend/src/features/cart/useAddToCart.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import { addToCart as addToCartApi } from '../../services/apiCart';
+export function useAddToCart() {
+  const queryClient = useQueryClient();
+  const { mutate: addToCart, isPending: isLoading } = useMutation({
+    mutationFn: addToCartApi,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['user'],
+      });
+      toast.success('Product added to cart successfully');
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { addToCart, isLoading };
+}
