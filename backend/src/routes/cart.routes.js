7416d86e2383e71d05fea73a6473c9e74893ff5a
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-18 23:34:03 +0530

    refactor: resolve HTTP methods issue

--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -11,6 +11,6 @@ const router = express.Router();
 router.get('/', protectRoute, getCartProducts);
 router.post('/', protectRoute, addToCart);
 router.delete('/', protectRoute, removeAllFromCart);
-router.put('/:productId', protectRoute, updateQuantity);
+router.patch('/:productId', protectRoute, updateQuantity);
 
 export default router;
