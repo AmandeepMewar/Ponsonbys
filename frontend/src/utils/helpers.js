364e6e16d5c0ec8f412c75f1efd069657c7810f4
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-18 23:08:38 +0530

    feat: add calculateTotals function

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,10 +1,27 @@
 export function formatCurrency(price) {
+  const hasDecimal = price % 1 !== 0;
+
   const formattedPrice = new Intl.NumberFormat('en', {
     style: 'currency',
     currency: 'INR',
-    minimumFractionDigits: 0,
+    minimumFractionDigits: hasDecimal ? 2 : 0,
     maximumFractionDigits: 2,
   }).format(price);
 
   return formattedPrice;
+}
+
+export function calculateTotals(cart, coupon, isCouponApplied) {
+  const subTotal = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
+  let total = subTotal;
+
+  if (coupon && isCouponApplied) {
+    const discount = subTotal * (coupon.discountPercentage / 100);
+    total = subTotal - discount;
+  }
+
+  return { total, subTotal };
 }
