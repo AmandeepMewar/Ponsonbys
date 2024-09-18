9efe9bd1578fcac1f29cc86ec00db30592792c37
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-18 23:06:52 +0530

    feat: add functionality  to updateItemQuantity component

--- frontend/src/features/cart/UpdateItemQuantity.jsx
+++ frontend/src/features/cart/UpdateItemQuantity.jsx
@@ -1,15 +1,26 @@
 import { Minus, Plus } from 'lucide-react';
+import Button from '../../ui/Button';
+import { useUpdateQuantity } from '../cart/useUpdateQuantity';
 
-export default function UpdateItemQuantity({ quantity }) {
+export default function UpdateItemQuantity({ quantity, productId }) {
+  const { updateQuantity, isLoading } = useUpdateQuantity();
   return (
     <div className='flex items-center gap-2 md:gap-3'>
-      <button className='flex h-10 w-10 items-center justify-center rounded-full bg-orange-500 hover:bg-orange-600'>
+      <Button
+        className='flex h-10 w-10 items-center justify-center rounded-full bg-orange-500 hover:bg-orange-600'
+        onClick={() => updateQuantity({ quantity: quantity - 1, productId })}
+        disabled={isLoading}
+      >
         <Minus className='h-5 w-5 text-yellow-50' />
-      </button>
+      </Button>
       <span className='text-lg font-medium text-yellow-800'>{quantity}</span>
-      <button className='flex h-10 w-10 items-center justify-center rounded-full bg-orange-500 hover:bg-orange-600'>
+      <Button
+        className='flex h-10 w-10 items-center justify-center rounded-full bg-orange-500 hover:bg-orange-600'
+        onClick={() => updateQuantity({ quantity: quantity + 1, productId })}
+        disabled={isLoading}
+      >
         <Plus className='h-5 w-5 text-yellow-50' />
-      </button>
+      </Button>
     </div>
   );
 }
