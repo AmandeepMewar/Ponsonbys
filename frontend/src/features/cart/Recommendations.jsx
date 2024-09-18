68fd1834a8a0eb0ff2fc904ba2a3aee8b1911a32
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-18 22:51:13 +0530

    feat: implement Recommendations  component

--- frontend/src/features/cart/Recommendations.jsx
+++ frontend/src/features/cart/Recommendations.jsx
@@ -1,3 +1,19 @@
+import ProductItem from '../category/ProductItem';
+import { useRecommendations } from './useRecommendations';
+
 export default function Recommendations() {
-  return <div></div>;
+  const { products: recommendations = [] } = useRecommendations();
+  return (
+    <div className='mt-10'>
+      <h3 className='text-2xl font-semibold text-yellow-800'>
+        You Might Also Like
+      </h3>
+
+      <div className='mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+        {recommendations.map((product) => (
+          <ProductItem key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
