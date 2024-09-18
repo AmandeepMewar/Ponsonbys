f14f6fac1d89cd663f40149d795101df994d7974
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-18 23:49:35 +0530

    style: add disabled styles

--- frontend/src/ui/Button.jsx
+++ frontend/src/ui/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, className, ...props }) {
   const classes =
     className ||
-    'flex w-full justify-center rounded-md border border-transparent bg-yellow-700 px-4 py-2 text-sm font-medium text-yellow-50 shadow-sm transition duration-150 ease-in-out hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 disabled:opacity-50';
+    'flex w-full justify-center rounded-md border border-transparent bg-yellow-700 px-4 py-2 text-sm font-medium text-yellow-50 shadow-sm transition duration-150 ease-in-out hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed';
   return (
     <button {...props} className={classes}>
       {children}
