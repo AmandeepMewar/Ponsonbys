fca16d264184b5843eaf6dd9a6345e2c74719122
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-19 22:28:34 +0530

    feat: implement useCheckoutSuccess  hook

---
+++ frontend/src/features/cart/useCheckoutSuccess.js
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { checkoutSuccess as checkoutSuccessApi } from '../../services/apiStripe';
+
+export function useCheckoutSuccess() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: checkoutSuccess,
+    isPending: isLoading,
+    data,
+  } = useMutation({
+    mutationFn: checkoutSuccessApi,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['cart'] });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+      navigate('/');
+    },
+  });
+
+  return { checkoutSuccess, isLoading, data };
+}
