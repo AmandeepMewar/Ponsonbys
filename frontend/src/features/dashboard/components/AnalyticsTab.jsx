67b6deb68dfd1b4426aeb58a5fcbf8e006e81840
Amandeep Mewar <amanmewar1718@gmail.com>
2024-09-20 17:33:13 +0530

    feat: implement AnalyticsTab component

--- frontend/src/features/dashboard/components/AnalyticsTab.jsx
+++ frontend/src/features/dashboard/components/AnalyticsTab.jsx
@@ -1,3 +1,34 @@
+import { motion } from 'framer-motion';
+
+import Loader from '../../../ui/Loader';
+import { useAnalytics } from '../hooks/useAnalytics';
+import AnalyticsChart from './AnalyticsChart';
+import Stats from './Stats';
+
 export default function AnalyticsTab() {
-  return <div>AnalyticsTab</div>;
+  const { data, isLoading } = useAnalytics();
+
+  const { analyticsData = {}, dailySalesData = [] } = data || {};
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+      <Stats analyticsData={analyticsData} />
+      <motion.div
+        className='rounded-lg bg-yellow-100 p-6 shadow-lg'
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        <div className='p-4'>
+          <h1 className='mb-4 text-2xl font-semibold'>
+            Sales Revenue Insights from the Last 7 Days
+          </h1>
+
+          <AnalyticsChart dailySalesData={dailySalesData} />
+        </div>
+      </motion.div>
+    </div>
+  );
 }
